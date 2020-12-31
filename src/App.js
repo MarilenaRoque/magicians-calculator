28c054dce059ec9072a6b7da4479c103da70865b
MarilenaRoque <marilenas@id.uff.br>
2020-12-31 11:18:29 -0300

    creating heroku app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-function App() {
+import PropTypes from 'prop-types';
+
+function App(props) {
   return (
     <div className="App">
-     <h1>Milestone I</h1>
+     <h3>Testing typechecking props</h3>
+     <h1>{props.title}</h1>
      <h1>Magician Calculator Project</h1>
+     
     </div>
   );
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title={150} />
   </React.StrictMode>,
   document.getElementById('root')
 );
