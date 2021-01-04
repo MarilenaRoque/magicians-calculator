47bdd3f96c73459f9643c91854bdcbf54a2d3d32
MarilenaRoque <marilenas@id.uff.br>
2021-01-04 19:14:02 -0300

    Add linters files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import PropTypes from 'prop-types';
-
-function App(props) {
+function App() {
   return (
     <div className="App">
-     <h3>Testing typechecking props</h3>
-     <h1>{props.title}</h1>
-     <h1>Magician Calculator Project</h1>
-     
+      <h1>Magician Calculator Project</h1>
+
     </div>
   );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App title={150} />
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
