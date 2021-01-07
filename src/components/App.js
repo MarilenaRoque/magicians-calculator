55decc0fd397b0932de00269a30342baa9a0d3ea
MarilenaRoque <marilenas@id.uff.br>
2021-01-07 13:09:56 -0300

    Turn App a class component
    change for class syntax
    set attributes
    pass attributes for display component

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
+import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate';
+// import calculate from '../logic/calculate';
 
-calculate({
-  total: '',
-  next: '80',
-  operation: '',
-}, '%');
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operator: null,
+    };
+  }
 
-function App() {
-  return (
-    <>
-      <Display />
-      <ButtonPanel />
-    </>
-  );
+  render() {
+    const { total, next, operator } = this.state;
+    return (
+      <>
+        <Display content={next} total={total} operator={operator} />
+        <ButtonPanel />
+      </>
+    );
+  }
 }
-
-export default App;
