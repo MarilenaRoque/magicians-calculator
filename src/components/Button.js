1317973ed4c83e06208c312e821872ab5ead9001
MarilenaRoque <marilenas@id.uff.br>
2021-01-14 17:26:08 -0300

    Styling Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,20 @@ import PropTypes from 'prop-types';
 
 function Button(props) {
   const { name, clickHandler } = props;
+
+  const orangeButtons = ['÷', 'X', '-', '+', '='];
+
+  const buttonColor = name => (orangeButtons.includes(name) ? '#F5913E' : '#E0E0E0');
+
+  const buttonStyle = {
+    height: '110px',
+    backgroundColor: buttonColor(name),
+    width: name !== '0' ? '25%' : '50%',
+  };
+
   return (
     <React.StrictMode>
-      <button type="button" onClick={() => clickHandler(name)}>{name}</button>
+      <button type="button" onClick={() => clickHandler(name)} style={buttonStyle}>{name}</button>
     </React.StrictMode>
   );
 }
