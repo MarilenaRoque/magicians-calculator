ef08547ae3aa446941b8a46b68d3e0432d5f907f
MarilenaRoque <marilenas@id.uff.br>
2021-01-15 14:09:46 -0300

    Calculator page
    Importing the app object
    set text

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import App from './App';
 
 export default function Calculator() {
+  const calculatorStyle = {
+    padding: '10px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  };
   return (
-    <>
-        
-    </>
+    <div style={calculatorStyle}>
+      <p style={{ fontSize: '30px' }}> Let’s do some math! </p>
+      <App />
+    </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
 export default function Home() {
+  const homeStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '70%',
+    margin: 'auto',
+  };
+  const textStyle = {
+    fontSize: '25px',
+  };
   return (
-    <div />
+    <div style={homeStyle}>
+      <h2 style={textStyle}>
+        Welcome to Math Magicians!
+      </h2>
+      <p style={textStyle}>
+        Math magicians is the tool that you needed to solve your mathematical problems in an easy,
+        faster and fun way. We want to help you and guide you through the Magic Math Land
+        and make you explore the best mathematical experiences.
+      </p>
+      <p style={textStyle}>
+        Okay...I admit that was corny, but we all know math isn’t always the most exciting
+        subject to teach. That means you have to find strategies to make lessons fun,
+        like gamification in the classroom, math puzzles or -- in this case -- math jokes
+        that will lighten the mood and brighten the vibe in your classroom.
+      </p>
+    </div>
   );
 }
