08049cc89020562af8d19380ce0474ebfdf3543d
MarilenaRoque <marilenas@id.uff.br>
2021-01-15 14:20:54 -0300

    Build quote view

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 export default function Quote() {
+  const quoteStyle = {
+    display: 'flex',
+    displayDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '70vh',
+  };
   return (
-    <div />
+    <div style={quoteStyle}>
+      <h3 style={{ fontSize: '30px' }}>What did the triangle say to the circle? “You’re pointless.”</h3>
+    </div>
   );
 }
